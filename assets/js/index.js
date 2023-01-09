c72d9836234056ab1850129cbd9b77e6e04afa6b
kkishan1230 <kkishan1230@gmail.com>
2023-01-09 15:54:34 +0530

    this is initial commmit

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+$(".home-card-carousel .carousel").slick({
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: `<button class="prev-arrow flex align-center justify-center"><img src="./assets/images/home-page/prevBlackArrow.svg" /></button>`,
+  nextArrow: `<button class="next-arrow flex align-center justify-center"><img src="./assets/images/home-page/nextBlackArrow.svg" /></button>`,
+});
