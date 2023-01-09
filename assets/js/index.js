8259c693ec69a222855f8d61cc192facbd6b5b5d
kkishan1230 <kkishan1230@gmail.com>
2023-01-09 17:20:26 +0530

    box carousel

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,18 @@
-$(".home-card-carousel .carousel").slick({
-  infinite: false,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  arrows: true,
-  prevArrow: `<button class="prev-arrow flex align-center justify-center"><img src="./assets/images/home-page/prevBlackArrow.svg" /></button>`,
-  nextArrow: `<button class="next-arrow flex align-center justify-center"><img src="./assets/images/home-page/nextBlackArrow.svg" /></button>`,
+$(document).ready(function () {
+  $(".home-card-carousel .carousel").slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: `<button class="prev-arrow flex align-center justify-center"><img src="./assets/images/home-page/prevBlackArrow.svg" /></button>`,
+    nextArrow: `<button class="next-arrow flex align-center justify-center"><img src="./assets/images/home-page/nextBlackArrow.svg" /></button>`,
+  });
+  // box carousel
+  $(".box-carousel-container").slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    centerMode: true,
+  });
 });
