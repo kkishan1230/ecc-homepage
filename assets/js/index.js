0e12064fc44459ab0fb0fe2b718c8dc89a18abd5
kkishan1230 <kkishan1230@gmail.com>
2023-01-10 11:14:59 +0530

    box carousel done

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,10 @@ $(document).ready(function () {
     infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
     centerMode: true,
+    variableWidth: true,
+    prevArrow: `<button class="prev-arrow flex align-center justify-center"><img src="./assets/images/home-page/prevBlackArrow.svg" /></button>`,
+    nextArrow: `<button class="next-arrow flex align-center justify-center"><img src="./assets/images/home-page/nextBlackArrow.svg" /></button>`,
   });
 });
